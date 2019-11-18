27a484ce7256c1420719da3bcb24eeeb0c35010d
bennettmountain <bennettmountain@gmail.com>
2019-11-17 23:11:40 -0800

    try 1

--- get_data.py
+++ get_data.py
@@ -4,38 +4,160 @@ import os
 import ast
 import matplotlib.pyplot as pyplot
 import textblob
+from textblob import TextBlob
+import lzma
+import zstandard
 import nltk
 import numpy as np
 import pandas as pd
-# list of subreddits to be considered
-subreddits = ['politics'] # adding more later after preliminary test
-# blank json file that i need to make in the repo?
+from textblob import TextBlob
+from textblob import Blobber
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+from PIL import Image
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 
-# for filename in os.listdir(directory), where directory is "/data/files.pushshift.io/reddit/submissions" to go through each file,
-# but need to have cases for each file type since not all of them are bz2
+subreddit_list = ['politics','uspolitics','americanpolitics','progressive','democrats','Liberal','Republican',
+                    'Conservative','Libertarian']
 
-f = os.chdir(~/data/files.pushshift.io/reddit/submissions)
-for i in f:
-    if i.endswith('.bz2'):
-        with bz2.open(filename:/data/files.pushshift.io/reddit/submissions, "r") as content:
-            # loop through each line (each line is a dict) and only save the posts that are in r/politics
-            # add each of those dicts to a new file (bz2 file?) and add it to my home directory
-            output = []
-            for i, line in enumerate(content):
-                post = json.loads(line)
-                if post.get("subreddit") in subreddits: 
-                    output.append(post)
-            # add the whole dict to the new file
+subreddit_members = {'politics':5.5E6,'uspolitics':1.65E4,'americanpolitics':1.12E4,'progressive':6.17E4,'democrats':1.11E5,
+                        'Liberal':7.78E4,'Republican':9.21E4,'Conservative':268E5,'Libertarian':345E5}
+                        
+# get more conservative and liberal words???
+liberal_words = [,"progressive","Biden","universal basic income","AOC", "Ocasio-Cortez", "liberal", "democrat", "Obama", "Clinton", "Sanders", 
+"green new deal", "leftist", "Yang", "Warren", "Kamala", "medicare for all"]
 
-# move output to a new file and move that to my home directory?
+conservative_words = ["Cheney","Shapiro","Koch","Paul Ryan","Rand Paul","Bush","Palin","Mattis","McCain","Romney", "Trump",
+"Cruz", "republican", "Kushner", "conservative", "GOP", "Pence"]
 
+# output is of the form {"subreddit_1_name":[('post_1_title',post_1_score),(''post_2_title',post_2_score),...], "subreddit_2_name"...}
+output = {}
+scores = {}
+aggregated_titles = {}
+bigram_count = {}
 
-liberal_words = ["liberal", "democrat, ""obama", "clinton", "sanders", "green new deal", "progressive", "leftist", ]
-conservative_words = ["trump", "white house", "cruz", "republican", "kushner", "conservative"]  
+def add_data():
+    '''
+    Parses through all the lines in a file (where each line in the file is a dict),
+    creates a tuple containing the title and score of the post, and adds it to the
+    list of tuples in its specified spot in the output dict.
+    '''
+    for line in content:
+        post = json.loads(line)
+        sub = post.get("subreddit")
+        if sub in subreddit_list:
+            if post.get("score") > 10: # arbitrary choice, should think about this more and change the threshold to be specific to each sub.
+                normalized_score = (post.get("score") * 1.0) / subreddit_members.get(sub)
+                if sub in output:
+                    output[sub].append((post.get("title"), post.get("score")))
+                else:
+                    output[sub] = [(post.get("title"), post.get("score"))]
 
-upvotes = dict.get("upvotes")
-title = blob(dict.get("title")) # or something like that
-sntmnt = title.sentiment()
-metric = (upvotes * 1.0) * sntmnt
+def open_files():
+    '''
+    TODO: figure out how to handle zst files
+    Goes through the directory containing all the data files.
+    '''
+    path = os.path.expanduser('~/data/files.pushshift.io/reddit/submissions')
+    files = [f for f in os.listdir(path)]
+    for i in files:
+        if i.endswith('.bz2'):
+            with bz2.open(i, "r") as content:  
+                add_data()
+        elif i.endswith('.xz'):
+            with lzma.open(i, 'rt') as content:
+                add_data()
+        # elif i.endswith('.zst'): # need to figure out how to open these.
+        #     with as content:
+        #         add_data()
 
-# how to deal with titles that have both conservative and liberal words?
+def aggregate_titles():
+    '''
+    Aggregate all the post titles for each subreddit
+    '''
+    for subreddit in output:
+        aggregated_titles[subreddit] = " ".join(j[0] for j in output[subreddit])
+        
+def create_metric():
+    '''
+    TODO: count number of conservative and liberal words. if num conservative > num liberal, multiply by -1.
+    Creates a bar graph with each subreddit and their aggregated political bias score
+    '''
+    for subreddit in output:
+        post_list = output[subreddit]
+        scores[subreddit] = 0
+            for j in post_list:
+                num_cons_words = 0
+                num_lib_words = 0
+                title_category_factor = 1 # 1 if title is about a liberal topic, -1 if about conservative topic
+                for word in j[0]:
+                    if word in num_cons_words:
+                        cons_words+=1
+                    elif word in num_lib_words:
+                        lib_words+=1
+                if num_cons_words >= num_lib_word:
+                    title_category = -1
+                title = blob(j[0])
+                if title.sentiment.subjectivity > 0.0:
+                    # heavier weighting for subjective article titles
+                    # since the min subjectivity > 0 is 0.1, multiplying by 50 gives it at least 5x weight
+                    sntmnt = (title.sentiment.polarity * 50.0 * title.sentiment.subjectivity)
+                else:
+                    sntmnt = title.sentiment.polarity
+                scores[subreddit] += ((j[1] * 1.0) + sntmnt) * title_category_factor
+    
+def create_bigrams():
+    '''
+    Creates a dict with the frequency of the bigrams for each subreddit
+    '''
+    for subreddit in aggregated_titles:
+        bigram_count_mini = {} # holds bigram frequencies for each subreddit
+        text = aggregated_titles[subreddit]
+        text = re.sub('\W', ' ', sentence.lower())
+        words = text.split()
+        bi_grams = list(zip(words, words[1:]))
+        for gram in bi_grams:
+            if gram not in bigram_count_mini:
+                bigram_count_mini[gram] = 1
+            else:
+                bigram_count_mini[gram]+=1
+        bigram_count[subreddit] = bigram_count_mini
+
+def plot_bigrams():
+    for subreddit in bigram_count:
+        bigram_string = subreddit + '_top_10_bigrams_.png'
+        top_10_bigrams_ = dict(sorted(x.items(), key=operator.itemgetter(1), reverse=True)[:10])
+        plt.bar(range(len(top_10_bigrams_)), list(top_10_bigrams_.values()), align='center')
+        plt.xticks(range(len(top_10_bigrams_)), list(top_10_bigrams_.keys()))
+        plt.savefig(bigram_string)
+
+def plot_metric():
+    plt.bar(range(len(scores)), list(scores.values()), align='center')
+    plt.xticks(range(len(scores)), list(scores.keys()))
+    plt.savefig('subreddit_scores.png')
+
+def plot_wordclouds():
+    '''
+    Creates a wordcloud for each subreddit
+    '''
+    for subreddit in aggregated_titles: # aggregate all titles of the subreddit in a dict
+        agg_text = aggregated_titles[subreddit]
+        stopwords= set(STOPWORDS)
+        wordcloud = WordCloud(stopwords=stopwords,max_font_size=50, max_words=100, background_color="white").generate(agg_text)
+        plt.figure()
+        plt.imshow(wordcloud, interpolation="bilinear")
+        plt.axis("off")
+        wordcloud_string = subreddit + '_wordcloud.png'
+        wordcloud.to_file(wordcloud_string)
+
+def main():
+    open_files()
+    for subreddit in output:
+        aggregate_titles()
+        create_metric()
+    for subreddit in aggregated_titles:
+        create_bigrams
+    plot_bigrams()
+    plot_metric()
+    plot_wordclouds()
