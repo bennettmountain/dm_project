873358723e10600835997f6acdc7f85507374920
bennettmountain <bennettmountain@gmail.com>
2019-12-12 11:22:49 -0800

    trying something new for output_Dates

--- get_data.py
+++ get_data.py
@@ -47,7 +47,7 @@ args = parser.parse_args()
 '''
 
 subreddit_list = ['politics','uspolitics','AmericanPolitics','progressive','democrats','Liberal','Republican',
-                    'Conservative','Libertarian']
+                    'Conservative','Libertarian', 'The_Donald']
 
 subreddit_members = {'politics':5.5E6,'uspolitics':1.65E4,'AmericanPolitics':1.12E4,'progressive':6.17E4,'democrats':1.11E5,
                         'Liberal':7.78E4,'Republican':9.21E4,'Conservative':268E5,'Libertarian':345E5}
@@ -106,7 +106,7 @@ def open_files():
     #files = [f for f in os.listdir(path)] #issue with RS_2011-01.bz2 having some non unicode-32 characters.
     #files = ['RS_2017-11.bz2','RS_2017-10.bz2','RS_2017-08.bz2','RS_2017-07.bz2','RS_2017-06.bz2','RS_2017-05.bz2','RS_2017-04.bz2']
     #files = ['RS_2011-01.bz2', 'RS_2012-01.bz2','RS_2013-01.bz2','RS_2014-01.bz2','RS_2015-01.gz','RS_2016-01.gz','RS_2017-01.bz2','RS_2018-01.xz','RS_2019-01.gz']
-    files = ['RS_2017-07.xz', 'RS_2018-01.xz']
+    files = ['RS_2011-01.bz2']
     with open("/home/bmountain/dm_project/output.json", "r+") as json_file:
         data = json.load(json_file)
         print('the current dates in the output are: ')
@@ -141,9 +141,11 @@ def open_files():
                                             log_normalized_score = (math.log(post.get("score")) * 1.0) / subreddit_members.get(sub)
                                             if sub in data["output_dateless"]: # sub also has to be in data[ouput_dates]
                                                 data["output_dateless"][sub].append([post.get("title"), log_normalized_score])
+                                            if sub in data["output_dates"][date]:
                                                 data["output_dates"][date][sub].append([[post.get("title"), log_normalized_score]])
-                                            else:
+                                            if sub not in data["output_dateless"]:
                                                 data["output_dateless"][sub] = [[post.get("title"), log_normalized_score]]
+                                            if sub not in data["output_dates"][date]:
                                                 data["output_dates"][date][sub] = [[post.get("title"), log_normalized_score]]
                                 except:
                                     pass
@@ -258,6 +260,9 @@ def create_scores_for_each_date():
         for date in dates:
             scores_dates[date] = []
             subreddit_dict = dates[date] # dict where each key is a sub
+            for subreddit_in_original_list in subreddit_list:
+                if subreddit_in_original_list not in subreddit_dict:
+                    scores_dates[date].append((subreddit_in_original_list, 0))
             for sub in subreddit_dict:
                 posts = subreddit_dict[sub]
                 score = 0
